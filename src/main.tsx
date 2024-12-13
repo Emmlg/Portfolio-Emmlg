d0393071c7b45d3dfa0db28735307673af6b3301
Emmanuel <lopez3mm4nu3l@gmail.com>
2024-12-13 00:37:49 -0600

    update(main): add footer property

--- src/main.tsx
+++ src/main.tsx
@@ -4,24 +4,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-// Importa las páginas que se renderizarán en las rutas
-import Navbar from './Components/NavBar';  // Asegúrate de importar el Navbar 
+// Importa las páginas y componentes
+import Navbar from './Components/NavBar';
+import Footer from './Components/Footer'; // Importa el Footer
 import PIndex from './pages/PIndex';
 import Experiencia from './pages/Experiencia';
 import MyWork from './pages/MyWork';
 import Contactame from './pages/Contactame';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>  
-      <Navbar />  {/* El Navbar se renderiza siempre en la parte superior */}
-      <Routes>  
-      <Route path="/" element={<PIndex />} /> 
-      <Route path="/experiencia" element={<Experiencia />} />
-        <Route path="/mitrabajo" element={<MyWork />} />
-        <Route path="/contactame" element={<Contactame />} />
-      </Routes>
+    <Router>
+      <Navbar /> {/* Navbar se renderiza siempre en la parte superior */}
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<PIndex />} />
+          <Route path="/experiencia" element={<Experiencia />} />
+          <Route path="/mitrabajo" element={<MyWork />} />
+          <Route path="/contactame" element={<Contactame />} />
+        </Routes>
+      </div>
+      <Footer /> {/* Footer se renderiza siempre en la parte inferior */}
     </Router>
   </StrictMode>
 );
