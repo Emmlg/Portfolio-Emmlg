16aaa2302e551b09cba682cb21be755b11b22457
Emmanuel <lopez3mm4nu3l@gmail.com>
2024-12-13 17:26:39 -0600

    fix(eslintconfig): fix strict mode= false

--- eslint.config.js
+++ eslint.config.js
@@ -11,4 +11,9 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off", // Desactiva la regla que requiere React en el alcance
+    },
+  },
 ];
